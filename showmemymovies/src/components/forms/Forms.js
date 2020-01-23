61f7cded82a8d3e403978956eb16f2fb6efc42e0
bleaz01 <ridjea@gmail.com>
2020-01-23 14:47:49 +0100

    forms done

--- showmemymovies/src/components/forms/Forms.js
+++ showmemymovies/src/components/forms/Forms.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import { Formik } from 'formik'
+import { Formik, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup';
 
+
+const SetField = ({ field, form, ...props }) => {
+    return (
+        <div className='form-group'>
+            <label>{field.name}</label>
+            <input {...field} {...props} type='text' className='form-control'></input> {/*par défaul le type sera text sinon {...props} qui donnera le type  */}
+        </div>
+    )
+}
+const Custom = (props) => {
+    return (
+        <div className='text-danger'>{props.children}</div>
+    )
+
+}
 export default class Forms extends React.Component {
 
     submit = (values) => {
         console.log(values)
     }
+    // Yup nous permet un controle sur les erreur et les autorisation du formulaire
     userSchema = Yup.object().shape({
-        user: Yup.string().min(3, 'trop court').required('champs obligatoir'),
-        email: Yup.string().email('mauvais email').required('champs obligatoir'),
-        password: Yup.string().min(5, 'trop cours')
+        user: Yup.string().min(3, 'trop court').required('champ obligatoire'),
+        email: Yup.string().email('mauvais email').required('champ obligatoire'),
+        password: Yup.string().min(5, 'trop court')
     })
+    //Et sans le module Yup 
+    // ||
+    // \/
+
     // formValidate = (values) => {
     //     let errors = {}
     //     values.user.length < 3 ? errors.user = '* trops court' : errors.user = null
@@ -30,34 +50,36 @@ export default class Forms extends React.Component {
                     initialValues={{ user: '', email: '', password: '' }}
                     // validate={this.formValidate}
                     validationSchema={this.userSchema}
-                    validateOnBlur={false}
-                    validateOnChange={false} // validateOnchange et validateOnBlur sont = a false commme sa ils les erreur sont envoyé que lors que le formulaire est envoyé
+                // validateOnBlur={false}
+                // validateOnChange={false} // validateOnchange et validateOnBlur sont = a false comme ça les erreurs sont inscrites que lors que le formulaire est envoyé
                 >
                     {/* // les propriétés de Formik qui son obligatoir    */}
-                    {({ values,
-                        handleBlur,
-                        handleChange,
+                    {({ //values,
+                        //handleBlur,
+                        //handleChange,  relaod a chaque insertion de l'utilisateur 
                         handleSubmit,
                         isSubmitting,
-                        errors      //errors permet de choper les erreurs que l'on récupère sur validate
+                        //errors      errors permet de choper les erreurs que l'on récupère sur validate
                     }) => (
                             <form onSubmit={handleSubmit} className=' bg-success p-5 d-flex flex-column'>
-                                <div className='form-group'>
-                                    <label>User</label>
-                                    <input name='user' value={values.user} onChange={handleChange} onBlur={handleBlur} type='text' className='form-control'></input>
-                                    <span className='text-danger text-sm'>{errors.user}</span>
-                                </div>
-                                <div className='form-group'>
-                                    <label>E-mail</label>
-                                    <input name='email' value={values.email} onChange={handleChange} onBlur={handleBlur} type='email' className='form-control'></input>
-                                    <span className='text-danger text-sm'>{errors.email}</span>
 
-                                </div>
-                                <div className='form-group'>
+                                <Field name='user' component={SetField} />
+                                <ErrorMessage name='user' component={Custom} />
+                                <Field name='email' type='email' component={SetField} />
+                                <ErrorMessage name='email' component={Custom} />
+                                <Field name='password' type='password' component={SetField} />
+                                <ErrorMessage name='password' component={Custom} />
+
+
+                                {/* Exemple de formulaire sans le Field
+                                                ||
+                                                \/  */}
+
+                                {/* <div className='form-group'>
                                     <label>Password</label>
                                     <input name='password' value={values.password} onChange={handleChange} onBlur={handleBlur} type='password' className='form-control'></input>
                                     <span className='text-danger text-sm'>{errors.password}</span>
-                                </div>
+                                </div> */}
                                 <button type='submit' className='btn btn-small btn-warning' disabled={isSubmitting}> Send</button>
                                 {/* iSubmitting est un boolean que lors que le formulaire est envoyé il devient true donc disabled = true . Grace à cela on ne peut pas envoyer plusieurs fois le meme formulaire */}
                             </form>
